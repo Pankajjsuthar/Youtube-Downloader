843b6b3fa6adf4bced7bdaca59de7201ac110cbc
Pankaj Suthar <pankajsuthar27302@gmail.com>
2024-06-19 10:11:47 +0530

    working extension

--- index.js
+++ index.js
@@ -3,17 +3,34 @@ var app = express();
 var ytdl = require('ytdl-core');
 
 app.listen('3000', function(){
-	console.log("listening on 3000");
+    console.log("Listening on 3000");
 });
 
 app.get('/download', function(req, res) {
-	var link = req.query.url;
-	var format = req.query.format;
-	var quality = req.query.quality;
+    var link = req.query.url;
+    var format = req.query.format;
+    var quality = req.query.quality;
+    var filename = req.query.filename;
 
-	video = ytdl(link,{
-		format:format,
-		quality:quality,
-	});
-	video.pipe(res);
+    if (!link || !format || !quality || !filename) {
+        return res.status(400).send('Missing required query parameters');
+    }
+
+    try {
+        res.header('Content-Disposition', `attachment; filename="${filename}.${format}"`);
+
+        const video = ytdl(link, {
+            format: format,
+            quality: quality,
+        });
+
+        video.pipe(res);
+        video.on('error', (err) => {
+            console.error(err);
+            res.status(500).send('Error downloading video');
+        });
+    } catch (err) {
+        console.error(err);
+        res.status(500).send('Server error');
+    }
 });
--- src/background.js
+++ src/background.js
@@ -1,23 +1,31 @@
 chrome.runtime.onInstalled.addListener(function() {
-	chrome.declarativeContent.onPageChanged.removeRules(undefined, function() {
-		chrome.declarativeContent.onPageChanged.addRules([{
-			conditions: [
-				new chrome.declarativeContent.PageStateMatcher({
-					pageUrl: { hostContains: 'youtube'}
-				})
-			],
-			actions: [ new chrome.declarativeContent.ShowPageAction() ]
-		}]);
-	});
+    chrome.declarativeContent.onPageChanged.removeRules(undefined, function() {
+        chrome.declarativeContent.onPageChanged.addRules([{
+            conditions: [
+                new chrome.declarativeContent.PageStateMatcher({
+                    pageUrl: { hostContains: 'youtube' }
+                })
+            ],
+            actions: [ new chrome.declarativeContent.ShowPageAction() ]
+        }]);
+    });
 });
 
-chrome.runtime.onMessage.addListener(function(message) {
-	var url = 'http://localhost:3000/download?';
-	var queryString = Object.keys(message).map(key => key + '=' + message[key]).join('&');
-	url += queryString;
-	console.log(url);
-	chrome.downloads.download({url:url,
-		filename: "YoutubeDownloader/" + message.filename +'.' + message.format}, function(downID) {
-			chrome.downloads.show(downID);
-	});
+chrome.runtime.onMessage.addListener(function(message, sender, sendResponse) {
+    var url = 'http://localhost:3000/download?';
+    var queryString = Object.keys(message).map(key => key + '=' + encodeURIComponent(message[key])).join('&');
+    url += queryString;
+
+    console.log(url);
+
+    chrome.downloads.download({
+        url: url,
+        filename: "YoutubeDownloader/" + message.filename + '.' + message.format
+    }, function(downloadId) {
+        if (downloadId) {
+            chrome.downloads.show(downloadId);
+        } else {
+            console.error('Download failed');
+        }
+    });
 });
